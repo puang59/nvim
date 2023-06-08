644034609379be2a11a00433c3916dcec86249da
puang59 <kk61468@gmail.com>
2023-06-08 13:06:19 +0530

    added lazgit toggle

--- lua/puang/core/keymaps.lua
+++ lua/puang/core/keymaps.lua
@@ -36,7 +36,7 @@ keymap.set("n", "<leader>tp", ":tabp<CR>") --  go to previous tab
 ----------------------
 
 -- toggleTerm
-keymap.set("n", "<leader>l", ":lua _LAZYGIT_TOGGLE()")
+keymap.set("n", "<leader>l", ":lua _LAZYGIT_TOGGLE()<CR>")
 
 -- vim-maximizer
 keymap.set("n", "<leader>sm", ":MaximizerToggle<CR>") -- toggle split window maximization
