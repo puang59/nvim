765606fad8795facf8beda1990cd6c72d10d7159
puang59 <kk61468@gmail.com>
2023-06-15 08:50:04 +0530

    shifted to nvchad

--- init.lua
+++ init.lua
@@ -1,21 +1,21 @@
-require("puang.plugins-setup")
-require("puang.core.options")
-require("puang.core.keymaps")
-require("puang.core.colorscheme")
-require("puang.plugins.comment")
-require("puang.plugins.nvim-tree")
-require("puang.plugins.lualine")
-require("puang.plugins.telescope")
-require("puang.plugins.nvim-cmp")
-require("puang.plugins.lsp.mason")
-require("puang.plugins.lsp.lspsaga")
-require("puang.plugins.lsp.lspconfig")
-require("puang.plugins.lsp.null-ls")
-require("puang.plugins.autopairs")
-require("puang.plugins.treesitter")
-require("puang.plugins.gitsigns")
-require("puang.plugins.bufferline")
-require("puang.plugins.org-mode")
-require("puang.plugins.indentline")
-require("puang.plugins.terminal")
--- require("puang.plugins.discord")
+require "core"
+
+local custom_init_path = vim.api.nvim_get_runtime_file("lua/custom/init.lua", false)[1]
+
+if custom_init_path then
+  dofile(custom_init_path)
+end
+
+require("core.utils").load_mappings()
+
+local lazypath = vim.fn.stdpath "data" .. "/lazy/lazy.nvim"
+
+-- bootstrap lazy.nvim!
+if not vim.loop.fs_stat(lazypath) then
+  require("core.bootstrap").gen_chadrc_template()
+  require("core.bootstrap").lazy(lazypath)
+end
+
+dofile(vim.g.base46_cache .. "defaults")
+vim.opt.rtp:prepend(lazypath)
+require "plugins"
